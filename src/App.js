9288c7d36e6acd945dc7158bdf5de2bfeaf840ab
tyronemurg <tyronemurg@gmail.com>
2023-09-27 11:08:01 +0200

    Partials | Nav | Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated import statement
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
 import WordPressPosts from './WordPressPosts';
 import SinglePost from './SinglePost';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes> 
         <Route exact path="/" element={<WordPressPosts />} />
         <Route path="/post/:id" element={<SinglePost />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        <p className="text-center">&copy; Web Warrior</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
